62eecfc3479eb6f1358a09865bfae2b27e50b10a
tenvu <interstellarpossibility@gmail.com>
2026-01-15 07:27:51 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
 const { app, BrowserWindow } = require("electron");
+const path = require("path");
 
-app.whenReady().then(() => {
+function createWindow() {
   const win = new BrowserWindow({
-    width: 460,
-    height: 560,
+    width: 460,      
+    height: 560,     
     resizable: false,
+    frame: false,    
+    alwaysOnTop: true, 
     webPreferences: {
-      contextIsolation: true
-    }
+      preload: path.join(__dirname, "preload.js"),
+    },
   });
 
   win.loadFile("index.html");
+  win.setMenuBarVisibility(false); 
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
 });
